b39bcd6b1ff9a4708668f276c70528be83563986
Alexander Smotrikov <alexandersmotrikov@coherentsolutions.com>
2021-05-03 18:12:05 +0300

    Add custom validator and validator pattern workaround

--- src/app/users/components/add-user/add-user.component.ts
+++ src/app/users/components/add-user/add-user.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, ValidationErrors } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidationErrors, AbstractControl, ControlContainer, Form } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ValidationService } from './validation.service';
-
+//To do: check how add an additional message to email error field
+function gmailDomainValidator(control: FormControl) {
+  let email = control.value;
+  if(email && email.indexOf('@') != -1) {
+    let [_, domain] = email.split('@');
+      if(domain !== 'gmail.com') {
+        return {
+          emailDomain: {
+            parsedDomain: domain
+          }
+        }
+      }
+      return null;
+  }
+}
 
 @Component({
   selector: 'add-user',
@@ -18,13 +32,13 @@ export class AddUserComponent implements OnInit {
 
   ngOnInit(): void {
       this.addUserForm = new FormGroup({
-      firstName: new FormControl('', Validators.required,),
+      firstName: new FormControl('', Validators.required),
       lastName: new FormControl('', Validators.required),
       age: new FormControl('', [Validators.required, Validators.min(15), Validators.max(100)]),
       company: new FormControl('', Validators.maxLength(35)),
-      email: new FormControl('youraddress@', Validators.email),
+      email: new FormControl('youraddress@', [Validators.email, gmailDomainValidator, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
       department: new FormControl('', Validators.required),
-      photo: new FormControl(''),
+      photo: new FormControl(null, Validators.required),
       gender: new FormControl('Male')
     })
   }
@@ -58,4 +72,6 @@ export class AddUserComponent implements OnInit {
     let checked = control.invalid && control.touched;
     return checked; 
   }
+  
+  
 }
